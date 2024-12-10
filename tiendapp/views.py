af6aa55ea5ffc5a91b6a5de1a610cf3ce9c6bd31
Leonardo Rojas Escarez <arens.rojas@gmail.com>
2024-12-10 19:06:55 -0300

    martes 10

--- tiendapp/views.py
+++ tiendapp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from tiendapp.models import Product
 
 # Create your views here.
 def v_index(request):
+    products_db = Product.objects.all()
     context = {
-        'products': [None, None, None, None, None]
+        'products': products_db
     }
     return render(request, 'tiendapp/index.html', context)
 
@@ -13,8 +15,9 @@ def v_cart(request):
     }
     return render(request, 'tiendapp/cart.html', context)
 
-def v_product_detail(request):
+def v_product_detail(request, code):
+    product_obj = Product.objects.get(sku = code)
     context = {
-        
+        'product': product_obj
     }
     return render(request, 'tiendapp/product_detail.html', context)
